d72ef362ea1efcdd2f205b0c518019f801d4d53a
akrimnafhm <akrimnafhm@gmail.com>
2026-08-22 19:03:56 +0700

    fix footer yt bact

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -24,7 +24,7 @@
                     <li>
                         <a href="https://www.youtube.com/@BACT_Yogyakarta" target="_blank" rel="noopener" class="inline-flex items-center gap-2 hover:text-[#E19404] transition">
                             <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 24 24"><path d="M23.498 6.186a3.016 3.016 0 00-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 00.502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 002.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 002.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg>
-                            <span class="font-semibold">Basic Advance Course in Transfusion 2027</span>
+                            <span class="font-semibold">Basic Advanced Course in Transfusion 2027</span>
                         </a>
                     </li>
                     <li>
